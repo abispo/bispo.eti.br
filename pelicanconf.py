f14bef4c0471a038f8fbf0b90e62035f70e960e1
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2020-07-21 17:10:20 -0300

    Added pelican-blue theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = 'pelican-themes/pelican-blue'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
