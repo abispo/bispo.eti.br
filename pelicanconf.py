74e554d7e9143b6ec3618ec1f2206e1daa16e508
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2020-07-21 17:29:41 -0300

    Temporary version.

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,8 +26,8 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
