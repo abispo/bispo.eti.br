f6228b27150ff065e4e97853d2c8405ef88f6f79
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2026-01-27 20:51:19 -0300

    Adjust for render

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/bispo.eti.br",
   // Optimize for static generation
   trailingSlash: true,
   images: {
